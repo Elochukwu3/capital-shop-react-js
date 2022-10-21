6747206e57def6ae4dcd93cafb6da4e1141ba552
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-21 23:05:08 +0100

    saved

--- src/header/HeaderTwo.jsx
+++ src/header/HeaderTwo.jsx
@@ -1,23 +1,54 @@
 import React from 'react';
-import { Menu, List } from 'semantic-ui-react';
+import { Cart, Person, Search } from 'react-bootstrap-icons';
+import { Grid, Image, List } from 'semantic-ui-react';
 import './css-files/header.css'
 
 export default function HeaderTwo() {
   return (
     <nav>
-        <Menu widths={3} fluid>
-            <Menu.Item name='Home' />
-            <Menu.Item>
+        <Grid columns={3} container style={{justifyContent: 'space-around', padding:'1rem 0'}}>
+            <Grid.Column style={{width: 'auto'}}>
+              <Image src={''}/>
+            </Grid.Column>
+          <Grid.Column style={{width: 'auto', fontWeight:'bolder', fontSize:'1.2rem'}}>
+          <List horizontal>
+            <List.Item>
                 Home
-                <Menu.Item name='Men'/>
-                <Menu.Item name='Women'/>
-                <Menu.Item name='Baby Collection'/>
-                <Menu.Item name='Page'/>
-                <Menu.Item name='Blog'/>
-            </Menu.Item>
-            <Menu.Item name='Home' />
-             
-        </Menu>
+            </List.Item>
+            <List.Item>
+               Men
+            </List.Item>
+            <List.Item>
+                Women
+            </List.Item>
+            <List.Item>
+               Baby Collections
+            </List.Item>
+            <List.Item>
+               Pages
+            </List.Item>
+            <List.Item>
+               Blog
+            </List.Item>
+            <List.Item>
+              Contact
+            </List.Item>
+        </List>
+            </Grid.Column>  
+            <Grid.Column style={{width: 'auto'}}>
+                <List horizontal>
+                    <List.Item>
+                        <Search/>
+                    </List.Item>
+                    <List.Item>
+                        <Person/>
+                    </List.Item>
+                    <List.Item>
+                        <Cart />
+                    </List.Item>
+                </List>
+            </Grid.Column>
+        </Grid>
     </nav>
   )
 }
