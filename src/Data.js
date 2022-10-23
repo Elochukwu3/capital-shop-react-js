8cc3c4a9e39939c5c309490270742da5aac7793b
Elochukwu3 <oguajuromauld5@gmail.com>
2022-10-23 17:50:27 +0100

    saved main

--- src/Data.js
+++ src/Data.js
@@ -1,6 +1,21 @@
-const data ={
+const data =
+[{
     img:'.//images/ladyblue.png',
     text:' Pellentesque habitant morbi tristique senectus et netus et malesuada fames   ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,',
     boldText:' minimal menz style'
 
+},
+{
+    img:'.//images/styledollcolor.png',
+    text:' Pellentesque habitant morbi tristique senectus et netus et malesuada fames   ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,',
+    boldText:' minimal menz style'
+
+},
+{
+    img:'.//images/snowwhite.png',
+    text:' Pellentesque habitant morbi tristique senectus et netus et malesuada fames   ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,',
+    boldText:' minimal menz style'
+
 }
+]
+export default data;
--- src/body/Main.js
+++ src/body/Main.js
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {Image, Segment } from 'semantic-ui-react';
 import MainButton from './MainButton';
 import "../css-files/Main.css";
+import data from '../Data';
+
 
 export default function Main() {
-  return (
-    <div className='mainPage'>
-       <section> <Image src='/images/ladyblue.png'style={{position:'absolute', zIndex:'-1', height:'100%'}}/></section>
-
-        <section className='mainpageText'>
+    const [slider, setSlider] = useState(0);
+    useEffect(() => {
+    setInterval(() => { 
+        if (slider <= data.length - 1) {
+            setSlider(prev=> prev + 1)
+        }else if(slider >= data.length - 1){
+            setSlider(prev=> prev - 1)
+        } else{
+            
+        }
+         
+            // setSlider((prev)=> prev >= data.length - 1 ? prev= 0: prev + 1)    
+    }, 2000); 
+    
+      return () => clearInterval(2000);
+    }, [slider])
+    
+    const mapItems = data.map(({img,text, boldText}, index)=>{
+        return(
+            <div className={slider === index? 'active': 'deactive'} key={index}>
+                <section> <Image src={img} style={{position:'absolute', zIndex:'-1', height:'100%'}}/></section>
+                <section className='mainpageText'>
             <p>Fashion Sale</p>
             <b style={{textTransform:'capitalize'}}>
-                minimal menz style
+                {boldText}
             </b>
             <Segment basic textAlign='center' style={{padding:'0 8rem', opacity:'.6'}}>
-               Pellentesque habitant morbi tristique senectus et netus et malesuada fames
-            ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
+            {text}
             </Segment>
             <MainButton name="shop Now"/>
-        </section>
+        </section> 
+
+            </div>
+        )
+    })
+    
+  return (
+    <div className='mainPage'>
+    {mapItems}
     </div>
   )
 }
